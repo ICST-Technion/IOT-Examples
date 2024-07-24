134739d211222441c1493081ad656a6ac1664fca
ShiraMiller <168965157+ShiraMiller@users.noreply.github.com>
2024-07-24 15:09:49 +0300

    Add files via upload

---
+++ hardware unit tests/stepper/stepper/stepper.ino
@@ -0,0 +1,27 @@
+/*   
+ *   Basic example code for controlling a stepper without library
+ *      
+ *   by Dejan, https://howtomechatronics.com
+ */
+
+// defines pins
+#define stepPin 2
+#define dirPin 5 
+ 
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(700); 
+  }
+  delay(1000); // One second delay
+
+}
