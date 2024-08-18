d056b7947b3668a917f2e8b7e9248f3f1aaa79f9
wydmynd <tomsofer@gmail.com>
2024-08-18 13:22:40 +0300

    library message

--- Audio examples/I2S/I2Saudio_radio_test_48khz/I2Saudio_radio_test_48khz.ino
+++ Audio examples/I2S/I2Saudio_radio_test_48khz/I2Saudio_radio_test_48khz.ino
@@ -12,6 +12,8 @@
 // OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 //
+// This code requires AudioI2S library from  https://github.com/schreibfaul1/ESP32-audioI2S (install manually from ZIP file)
+
 
 #include "Arduino.h"
 #include "WiFiMulti.h"
